1975dbf79483ed28ea4bb126931fbaf3bb31bf39
vyacheslavStarokozhko <vyacheslav.s@thewhitelabelagency.com>
2024-03-05 14:17:54 +0200

    Page Update

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -17,6 +17,23 @@
 
         });
 
+        function InitAccordion() {
+            $('.accordion-labels li:first-child .accordion-button,.accordion-contents .accordion-content:first-child').addClass('is-active');
+            $('.accordion-button').click(Accordion);
+        }
+
+        function Accordion(event) {
+            $(`.accordion-content, .accordion-button`).removeClass('is-active');
+
+            let $this = $(event.target);
+            let idx = $this.data('idx');
+
+            $(`.accordion-content[data-idx=${idx}], .accordion-button[data-idx=${idx}]`).addClass('is-active');
+
+        }
+
+        InitAccordion()
+
 
     })
 })(jQuery);
